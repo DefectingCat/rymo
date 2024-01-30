58b7579862f568d6378c3de98ffea28025858abe
DefectingCat <i@defect.ink>
2024-01-30 14:23:16 +0800

    add read headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use anyhow::Result;
-use std::{collections::HashMap, future::Future};
-use tokio::net::TcpListener;
+use bytes::Bytes;
+use std::{collections::HashMap, future::Future, pin::Pin};
+use tokio::{
+    io::{AsyncBufReadExt, AsyncRead, AsyncReadExt, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 #[derive(Debug)]
 pub struct Rymo<'a, 'b, F, Fut>
 where
     F: FnOnce() -> Fut + 'static + Send + Sync,
-    Fut: Future<Output = ()>,
+    Fut: Future<Output = (i32, Bytes)>,
 {
     pub port: &'a str,
     pub handle: HashMap<&'b str, F>,
@@ -15,7 +19,7 @@ where
 impl<'a, 'b, F, Fut> Rymo<'a, 'b, F, Fut>
 where
     F: FnOnce() -> Fut + 'static + Send + Sync,
-    Fut: Future<Output = ()>,
+    Fut: Future<Output = (i32, Bytes)>,
 {
     pub fn new(port: &'a str) -> Self {
         Self {
@@ -28,11 +32,35 @@ where
         let listener = TcpListener::bind(format!("0.0.0.0:{}", self.port)).await?;
 
         loop {
-            let (_socket, _) = listener.accept().await?;
+            let (socket, _) = listener.accept().await?;
+            self.process(socket).await?;
         }
     }
 
     pub fn get(&mut self, path: &'b str, handler: F) {
         self.handle.entry(path).or_insert(handler);
     }
+
+    pub async fn process(&self, mut socket: TcpStream) -> Result<()> {
+        let (reader, _writer) = socket.split();
+
+        let headers = read_headers(reader).await?;
+        dbg!(&headers);
+        Ok(())
+    }
+}
+
+pub async fn read_headers<R>(reader: R) -> Result<String>
+where
+    R: AsyncRead + std::marker::Unpin,
+{
+    let mut request_string = String::new();
+    let mut reader = BufReader::new(reader);
+    loop {
+        let byte = reader.read_line(&mut request_string).await?;
+        if byte < 3 {
+            break;
+        }
+    }
+    Ok(request_string)
 }
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<()> {
     let port = env::var("PORT").unwrap_or("4000".into());
     info!("listening on {port}");
     let mut app = Rymo::new(&port);
-    app.get("/", || async move {});
+    app.get("/", || async move { (200, "Hello world".into()) });
     app.serve().await?;
     Ok(())
 }
