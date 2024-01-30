a6d0da86b794e5a6c57188b59aba1c7b0f7e9085
DefectingCat <i@defect.ink>
2024-01-30 09:32:44 +0800

    init logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use anyhow::Result;
+use dotenvy::dotenv;
+use logger::init_logger;
+use rymo::Rymo;
+use std::env;
+use tracing::{info, warn};
+
+mod logger;
+mod rymo;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    init_logger();
+    dotenv().map_err(|err| warn!("env file {err}")).ok();
+
+    let port = env::var("PORT")
+        .unwrap_or("4000".into())
+        .parse()
+        .unwrap_or(4000);
+    info!("listening on {port}");
+    Rymo::listen(port);
     Ok(())
 }
